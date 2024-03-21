e096a0e2c8751cc570fb88af7c0c0355631deb66
Seyed Danial Movahed <movaheddanial@gmail.com>
2024-03-21 14:56:33 +0330

    Added dashboard view and removed login and signup for now

--- app/controllers/main_controller.ts
+++ app/controllers/main_controller.ts
@@ -2,9 +2,6 @@ import type { HttpContext } from '@adonisjs/core/http'
 
 export default class MainController {
   async index(ctx: HttpContext) {
-    if (!(await ctx.auth.check())) return ctx.response.redirect('/login')
-
-    console.log(ctx.auth.user?.username)
-    return ctx.response.redirect('/dashboard')
+    return ctx.view.render('dashboard')
   }
 }
--- resources/js/dashboard.js
+++
@@ -1,6 +0,0 @@
-const socket = io()
-
-socket.on('msgFromBE', (data) => {
-  console.log('msgFromBE', data)
-  socket.emit('msgFromFE', { hello: 'from FE' })
-})
